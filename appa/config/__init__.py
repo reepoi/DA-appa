477a6d123cf2aa4d1793820b8388d9eced6ffc22
Taos Transue <spacechips@rocketmail.com>
2026-06-01 20:09:48 +0000

    fix paths

--- appa/config/__init__.py
+++ appa/config/__init__.py
@@ -10,8 +10,9 @@ from .hydra import compose
 
 PROJECT = Path(__file__).parent.parent.parent
 
-PATH_AE = PROJECT / "autoencoders"
+PATH_AE = PROJECT/"autoencoders"
 
-PATH_ERA5 = PROJECT / "data" / "era5_1993-2021-1h-1440x721.zarr"
-PATH_STAT = PROJECT / "data" / "stats_era5_1993-2021-1h-1440x721.zarr"
-PATH_MASK = PROJECT / "data" / "masks_era5_1993-2021-1h-1440x721.zarr"
+# PATH_ERA5 = PROJECT / "data" / "era5_1993-2021-1h-1440x721.zarr"
+PATH_ERA5 = Path("/vast/users/ac.ttransue/appa/era5/1959-2023_01_10-6h-240x121_equiangular_with_poles_conservative.zarr")
+PATH_STAT = PATH_ERA5.parent/f'stats_{PATH_ERA5.name}'
+PATH_MASK = PATH_ERA5.parent/f'masks_{PATH_ERA5.name}'
